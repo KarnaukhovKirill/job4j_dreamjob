817cfd8b915a096e0a74c3819c5935e2a6e1e7b4
KarnaukhovKirill <kirya2361@gmail.com>
2022-01-09 11:10:03 +0300

    change memstore to dbstore

--- src/main/java/ru/job4j/dream/servlet/CandidatePhotoDeleteServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidatePhotoDeleteServlet.java
@@ -1,9 +1,8 @@
 package ru.job4j.dream.servlet;
 
+import ru.job4j.dream.store.DbStore;
 import ru.job4j.dream.util.CandidatePhotoSearcher;
-import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.util.PhotoSearcher;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +14,7 @@ public class CandidatePhotoDeleteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var idCandidate = req.getParameter("id");
-        MemStore.instOf().delCandidate(Integer.parseInt(idCandidate));
+        DbStore.instOf().delCandidate(Integer.parseInt(idCandidate));
         PhotoSearcher searcher = new CandidatePhotoSearcher();
         var candidatePhoto = searcher.search(idCandidate);
         candidatePhoto.delete();
